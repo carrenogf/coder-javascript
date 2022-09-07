0b14e51f2bbb696c31337e976b933e78d478be4b
carrenogf <carrenogf@gmail.com>
2022-09-07 19:44:19 -0300

    Proyecto final primera entrega

---
+++ Proyecto final - Entrega 1/README.md
@@ -0,0 +1,32 @@
+# CODERHOUSE - Javascript Primera entrega del Proyecto Final
+# Francisco Salvador Carreño Giscafré
+
+## Calculo de un prestamo
+El objetivo es crear en base a los datos ingresados por el usuario, una simulación con los diferentes tipos de sistemas de amortización financieros.
+
+Datos requeridos al usuario:
+* sistema a utilizar (ingresar el numero de una lista)
+* Monto del capital
+* Tasa anual
+* Cantidad de periodos
+
+Los sistemas a calcular son:
+### Sistema Francés:
+* Interés calculado sobre saldos
+* Cuota constante
+* Amortización de capital creciente
+* Interés decreciente
+
+### Sistema Alemán:
+* Interés calculado sobre saldos
+* Cuota decreciente
+* Amortización de capital constante
+* Interés decreciente
+
+### Sistema Directo:
+* Cuota constante
+* Intereses constantes calculado sobre el saldo inicial
+* El interé de cada pago es constantea
+* Amortización constante
+
+El resultado se muestra por consola y se ingresan los datos requeridos por promts.
--- Proyecto final - Entrega 1/index.js
+++ Proyecto final - Entrega 1/index.js
@@ -1,7 +1,6 @@
 function imprimir(tabla) {
   // imprimir por consola las tablas
   console.log("Per", "Amort", "Int", "Pago", "Saldo");
-  let totalTabla = 0;
   tabla.forEach((renglon) => {
     console.log(
       renglon.per,
@@ -10,37 +9,7 @@ function imprimir(tabla) {
       renglon.pago.toFixed(2),
       renglon.saldo.toFixed(2)
     );
-    // aprovecho el primer bucle para calcular el total de la columna de amortización y luego ponderar a la mitad
-    totalTabla += renglon.amortizacion;
   });
-  // cantidad de periodos
-  const n = tabla.length;
-  // busqueda para obtener cuota del primer periodo
-  const per1 = tabla.find((c1) => c1.per === 1);
-  console.log(`La primer cuota será de $ ${per1.pago.toFixed(2)}`);
-  // busqueda para obtener cuota del último periodo
-  const pern = tabla.find((cn) => cn.per === n);
-  console.log(`La última cuota será de $ ${pern.pago.toFixed(2)}`);
-  // calculos para la mitad del prestamo
-  // la idea es que informe cuanto se va pagando de intereses y amortización a la mitad del prestamo
-  // mitad de periodos
-  const n2 = parseInt(n / 2);
-  // filtro para obtener los objetos de la tabla que corresponden a la mitad del prestamo
-  const mitad = tabla.filter((i) => i.per <= n2);
-  // variables para acumular los valores en el siguiente bucle
-  let amortMitad = 0;
-  let interesMitad = 0;
-  mitad.forEach((renglon) => {
-    amortMitad += renglon.amortizacion;
-    interesMitad += renglon.interes;
-  });
-  // porcentaje de amortización a la mitad del prestamo
-  let percentMitad = (amortMitad / totalTabla) * 100;
-  // impresiones de los calculas a la mitad del prestamo
-  console.log("Cuando hayas pagado la mitad de las cuotas:");
-  console.log(`Habras pagado un total de $ ${amortMitad.toFixed(2)} en concepto de amortización`);
-  console.log(`Habras pagado un total de $ ${interesMitad.toFixed(2)} en concepto de intereses`);
-  console.log(`Y habrás cancelado el ${percentMitad.toFixed(2)}% del total del prestamo`);
 }
 
 function amortizacion(sistema) {
@@ -120,7 +89,7 @@ let sistema = parseInt(
   )
 );
 while (sistema != 0 && sistema != 1 && sistema != 2 && sistema != "salir") {
-  sistema = parseInt(
+  let sistema = parseInt(
     prompt(
       "Ingresar el sistema para calcular:\n0 - para sistema frances\n1 - para sistema aleman\n2 - para sistema directo"
     )
@@ -139,7 +108,6 @@ if (sistema != "salir") {
   alert(
     `Calculo para sistema ${sistemas[sistema]} solicitado, porfavor revisar la consola!`
   );
-  console.log(`Calculo para sistema ${sistemas[sistema]}:`);
   calular_sistema(capital, tasa, n);
 }
 alert("Gracias por utilizar nuestro sitio!");
