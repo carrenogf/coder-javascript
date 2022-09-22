9d5f893556f1a371b17f9b4ac4177dcaf8c4caf9
carrenogf <carrenogf@gmail.com>
2022-09-21 23:08:43 -0300

    aplicado prettier

--- Proyecto final - Entrega 2/index.js
+++ Proyecto final - Entrega 2/index.js
@@ -1,44 +1,41 @@
-function renderTable(contenedor,encabezados, valores, toFixed=[]) {
+function renderTable(contenedor, encabezados, valores, toFixed = []) {
   let container = document.getElementById(contenedor);
-  container.innerHTML=""
+  container.innerHTML = "";
   let tabla = document.createElement("table");
-  tabla.className = "table table-bordered"
+  tabla.className = "table table-bordered";
   // encabezados
   let trh = document.createElement("tr");
 
-  for (const titulo of encabezados){
+  for (const titulo of encabezados) {
     let th = document.createElement("th");
-    th.innerHTML = titulo
-    th.setAttribute("scope","col")
-    trh.append(th)
+    th.innerHTML = titulo;
+    th.setAttribute("scope", "col");
+    trh.append(th);
   }
-  tabla.append(trh)
+  tabla.append(trh);
   // valores
-  console.log(valores)
+  console.log(valores);
   for (let renglon of valores) {
-    
     let tr = document.createElement("tr");
     let keys = Object.keys(renglon);
     for (let key of keys) {
       let td = document.createElement("td");
-      if (toFixed.includes(key)){
-        td.innerHTML = renglon[key].toFixed(2)
-      }else{
-        td.innerHTML = renglon[key]
+      if (toFixed.includes(key)) {
+        td.innerHTML = renglon[key].toFixed(2);
+      } else {
+        td.innerHTML = renglon[key];
       }
-      tr.append(td)
+      tr.append(td);
     }
-    tabla.append(tr)
+    tabla.append(tr);
   }
-  container.append(tabla)
+  container.append(tabla);
 }
-
-
 
 function imprimir(prestamo) {
   // imprimir las tablas
-  let encabezados = ["Periodo","Amortización","Intereses","Pago","Saldo"]
-  let toFixed = ["amortizacion","interes","pago","saldo"]
+  let encabezados = ["Periodo", "Amortización", "Intereses", "Pago", "Saldo"];
+  let toFixed = ["amortizacion", "interes", "pago", "saldo"];
 
   let totalPrestamo = 0;
   let totalIntereses = 0;
@@ -53,12 +50,12 @@ function imprimir(prestamo) {
     per: "Totales",
     amortizacion: totalPrestamo,
     interes: totalIntereses,
-    pago:totalPagos,
+    pago: totalPagos,
     saldo: 0,
   });
-  renderTable("tablaPrestamo",encabezados,prestamo,toFixed);
+  renderTable("tablaPrestamo", encabezados, prestamo, toFixed);
 }
-function informe(sistema,capital, n, tasa, prestamo) {
+function informe(sistema, capital, n, tasa, prestamo) {
   let container_informe = document.getElementById("informe");
   container_informe.innerHTML = "";
   // busqueda para obtener cuota del primer periodo
@@ -73,7 +70,7 @@ function informe(sistema,capital, n, tasa, prestamo) {
   let totalIntereses = 0;
   let totalPagos = 0;
   prestamo.forEach((renglon) => {
-    if ( renglon.per <= n2){
+    if (renglon.per <= n2) {
       amortMitad += renglon.amortizacion;
       interesMitad += renglon.interes;
     }
@@ -88,20 +85,26 @@ function informe(sistema,capital, n, tasa, prestamo) {
                       <p>La primer cuota será de $ ${per1.pago.toFixed(2)}</p>
                       <p>La última cuota será de $ ${pern.pago.toFixed(2)}</p>
                       <strong><p>Cuando hayas pagado la mitad de las cuotas:</p></strong>
-                      <p>Habras pagado un total de $ ${amortMitad.toFixed(2)} en concepto de amortización</p>
-                      <p>Habras pagado un total de $ ${interesMitad.toFixed(2)} en concepto de intereses</p>
-                      <p>Y habrás cancelado el ${percentMitad.toFixed(2)}% del total del prestamo</p>
+                      <p>Habras pagado un total de $ ${amortMitad.toFixed(
+                        2
+                      )} en concepto de amortización</p>
+                      <p>Habras pagado un total de $ ${interesMitad.toFixed(
+                        2
+                      )} en concepto de intereses</p>
+                      <p>Y habrás cancelado el ${percentMitad.toFixed(
+                        2
+                      )}% del total del prestamo</p>
                       <button onclick="comparador();" class="btn btn-primary" id="comparar">Comparar</button>`;
-                      
+
   container_informe.append(informe);
   return {
-    sistema:sistema,
-    capital:capital,
-    tasa:tasa,
-    periodos:n,
-    intereses:totalIntereses,
-    pago:totalPagos,
-  }
+    sistema: sistema,
+    capital: capital,
+    tasa: tasa,
+    periodos: n,
+    intereses: totalIntereses,
+    pago: totalPagos,
+  };
 }
 
 function frances(capital, tasa, n) {
@@ -182,57 +185,58 @@ function amortizacion(capital, tasa, n, sistema) {
   }
   return prestamo;
 }
-const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
+const guardarLocal = (clave, valor) => {
+  localStorage.setItem(clave, valor);
+};
 
-
-function comparador () {
-  
+function comparador() {
   let nuevoPrestamoLS = localStorage.getItem("nuevoPrestamo");
-  
+
   if (nuevoPrestamoLS) {
-    
     // N° de prestamos guardados
     let numPrestamos = parseInt(localStorage.getItem("numPrestamo"));
     if (numPrestamos) {
       numPrestamos += 1;
-      guardarLocal("numPrestamo",numPrestamos)
-      guardarLocal(`prestamo${numPrestamos}`,nuevoPrestamoLS)
-    }else{
-      
-      guardarLocal("numPrestamo",1)
-      guardarLocal(`prestamo1`,nuevoPrestamoLS)
+      guardarLocal("numPrestamo", numPrestamos);
+      guardarLocal(`prestamo${numPrestamos}`, nuevoPrestamoLS);
+    } else {
+      guardarLocal("numPrestamo", 1);
+      guardarLocal(`prestamo1`, nuevoPrestamoLS);
     }
-    renderComparador()
+    renderComparador();
   }
-
 }
 
-function renderComparador (){
+function renderComparador() {
   let numPrestamos = localStorage.getItem("numPrestamo");
   if (numPrestamos) {
-    encabezados = ["Sistema","Capital","Tasa","Periodos","Intereses","Pago"]
-    valores = []
-    for (let i=1; i<=numPrestamos ;i++){
-      
-      let prestamo_i =  localStorage.getItem(`prestamo${i}`);
+    encabezados = [
+      "Sistema",
+      "Capital",
+      "Tasa",
+      "Periodos",
+      "Intereses",
+      "Pago",
+    ];
+    valores = [];
+    for (let i = 1; i <= numPrestamos; i++) {
+      let prestamo_i = localStorage.getItem(`prestamo${i}`);
       if (prestamo_i) {
-        prestamo_i = JSON.parse(prestamo_i)
-      }else{
-        localStorage.removeItem(`prestamo${i}`)
+        prestamo_i = JSON.parse(prestamo_i);
+      } else {
+        localStorage.removeItem(`prestamo${i}`);
       }
-      valores.push(prestamo_i)
+      valores.push(prestamo_i);
     }
-    toFixed = ["capital","tasa","periodos","intereses","pago"]
+    toFixed = ["capital", "tasa", "periodos", "intereses", "pago"];
     renderTable(
-      contenedor="comparador",
-      encabezados=encabezados,
-      valores=valores,
-      toFixed=toFixed
-    )
-
+      (contenedor = "comparador"),
+      (encabezados = encabezados),
+      (valores = valores),
+      (toFixed = toFixed)
+    );
   }
 }
-
 
 function validarFormulario(e) {
   e.preventDefault();
@@ -243,17 +247,12 @@ function validarFormulario(e) {
   let prestamo = amortizacion(capital, tasa, n, sistema);
 
   imprimir(prestamo);
-  let comparar = informe(sistema,capital, n, tasa, prestamo);
+  let comparar = informe(sistema, capital, n, tasa, prestamo);
   // calculos para la comparacion
   // deberia devolver capital tasa n sistema total intereses total pagos
   guardarLocal("nuevoPrestamo", JSON.stringify(comparar));
-
 }
 
 localStorage.clear();
 let form = document.getElementById("formulario");
 form.addEventListener("submit", validarFormulario);
-
-
-
-
